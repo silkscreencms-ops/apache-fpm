2edfc37a6e6afa7ec5e9b508946f28f414f24a70
John Franklin <franklin@sentaidigital.com>
2018-07-22 11:45:55 -0400

    Add first cut of one-time.sh

---
+++ one-time.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+$DATABASE=mysql
+
+if [ $(id -u) -ne 0 ]; then
+	echo "Must run as root."
+	exit;
+fi
+
+apt-get update
+apt-get install -y apache2 apache2-suexec-custom libapache2-mod-fcgid php-fpm
+apt-get install -y php-gd php-json php-xml php-mbstring php-zip php-curl unzip git
+
+case $DATABASE in
+	mariadb|mysql)
+		$DB_PKGS=mariadb-client mariadb-server php-mysql
+	;;
+	pgsql|postgres)
+		$DB_PKGS=postgresql-server postgresql-client php-pgsql
+		$DB_DRIVER=database_pgsql
+	;;
+	sqlite|sqlite3)
+		$DB_PKGS=php-sqlite3
+		$DB_DRIVER=database_sqlite
+
+esac
+
+apt-get install -y $DB_PKGS
+
+if [ x$DRIVER != "x" ]; then 
+	# Fetch the latest drivers
+fi
