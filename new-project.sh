dbcdc9ce103c2a551d5bb6ea4d97184d4bdfdde3
John Franklin <franklin@sentaidigital.com>
2018-07-22 12:45:58 -0400

    Break on errors, create the project directories correctly.

--- new-project.sh
+++ new-project.sh
@@ -9,6 +9,8 @@ FPM_ROOT=/etc/php/7.0/fpm
 
 #### DON'T EDIT ANYTHING BELOW THIS LINE ####
 
+set -e
+
 if [ $(id -u) -ne 0 ]; then
 	echo "Must run as root."
 	exit;
@@ -42,7 +44,7 @@ if [ -d ${HOME}/${PROJECT} ]; then
 	echo "Project directory found, will not setup ${HOME}/${PROJECT}"
 else
 	## Setup Silkscreen
-	mkdir -p ${HOME}/${PROJECT}/{docroot,database,private,config}
+	mkdir -p ${HOME}/${PROJECT}/docroot ${HOME}/${PROJECT}/database ${HOME}/${PROJECT}/privat ${HOME}/${PROJECT}/config
 	curl -o /tmp/silkscreen.tgz https://github.com/silkscreencms/silkscreen/archive/silkscreen-${VERSION}.tar.gz
 	tar -C ${HOME}/${PROJECT}/docroot --strip-components=1 -xaf /tmp/silkscreen.tgz
 	rm /tmp/silkscreen.tgz
